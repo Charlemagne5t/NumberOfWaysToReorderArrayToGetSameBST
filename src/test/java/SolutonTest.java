cf233b4e89756b77e4f98b073cee738bbc07410b
alek <wait82985@gmail.com>
2023-06-16 12:30:49 +0600

    initial commit. 2 test cases

---
+++ src/test/java/SolutonTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutonTest {
+    @Test
+    public void numOfWaysTest1() {
+        int[] nums = {2, 1, 3};
+        int output = 1;
+        Assert.assertEquals(output, new Solution().numOfWays(nums));
+    }
+
+    @Test
+    public void numOfWaysTest2() {
+        int[] nums = {3, 4, 5, 1, 2};
+        int output = 5;
+        Assert.assertEquals(output, new Solution().numOfWays(nums));
+    }
+
+}
